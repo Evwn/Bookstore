544ba43166f8a0f1d9c1926d198d9d24b09e6bdb
Erastus W. Mututa <EMututa@fintech-group.com>
2026-03-12 15:46:27 +0300

    good

--- db.php
+++ db.php
@@ -1,22 +1,11 @@
 
 <?php
 
-$host = '20.185.147.112';
-$port = '1521';
-$sid  = 'xe';
-$user = 'C##yannick_mbolela';
-$pass = 'yannick4592';
-
 try {
-    $dsn = "oci:dbname=(DESCRIPTION=
-                (ADDRESS=(PROTOCOL=TCP)(HOST=$host)(PORT=$port))
-                (CONNECT_DATA=(SID=$sid))
-            )";
-
-    $pdo = new PDO($dsn, $user, $pass);
+    $pdo = new PDO('sqlite:bookstore.db');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    echo "Connected to Oracle successfully";
+    echo "Connected to SQLite successfully";
 
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
